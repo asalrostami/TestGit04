63282f53a5d6a5e71d3b192ee91907a619f61081
Asal Rostami <asal@Asals-MacBook-Pro.local>
2017-01-16 17:08:46 -0500

    initial commit

---
+++ TestGit04/TestGit04/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGit04
+//
+//  Created by Asal Rostami on 2017-01-16.
+//  Copyright © 2017 Joopooli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
